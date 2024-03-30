3d085d5847d76a1a237b0cf4d9b86539f6a89d41
Elif Çiftçi <eliffbat@gmail.com>
2024-03-30 23:55:10 +0300

    feat: jest configuration completed

--- src/component/atoms/WorkTogether/index.tsx
+++ src/component/atoms/WorkTogether/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 const WorkTogether = () => {
   return (
     <Link
+      data-testid="work-together"
       href={"/no-code"}
       className="animate-fade-in [animation-timeline:scroll()] [animation-range:0px_100px] max-h-[150px] h-[30vh] w-[100%] bg-black-default hover:bg-black-dark transition-all flex justify-center items-center font-semibold text-xl"
     >
