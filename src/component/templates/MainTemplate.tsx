10bc935474b2c05ccdc3ab3f0cfd5c8f9b7bb553
Elif Çiftçi <eliffbat@gmail.com>
2024-04-16 18:19:28 +0300

    fix: prevent scroll when navbar opened on mobile devices

--- src/component/templates/MainTemplate.tsx
+++ src/component/templates/MainTemplate.tsx
@@ -1,16 +1,42 @@
+"use client";
+
 import { inter } from "@/app/fonts";
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
 
 interface MainLayoutProps {
   children: ReactNode;
   className?: string;
 }
 
-// This is the place responsible for wrapping your app.
-// Add here components like Footer, Nav etc.
 export const MainLayout = ({ children, className }: MainLayoutProps) => {
+  const isOpenedMenu = useSelector(
+    (state: RootState) => state.toggleMenuSlice.isOpenedMenu
+  );
+
+  const preventDefault = (e: TouchEvent) => {
+    if (isOpenedMenu) {
+      e.preventDefault();
+    }
+  };
+
+  // prevent scroll when navbar opened on mobile devices 
+  React.useEffect(() => {
+    const body = document.body;
+
+    if (isOpenedMenu) {
+      body.addEventListener("touchmove", preventDefault, { passive: false });
+    } else {
+      body.removeEventListener("touchmove", preventDefault);
+    }
+
+    // Cleanup function to remove event listener on unmount
+    return () => body.removeEventListener("touchmove", preventDefault);
+  }, [isOpenedMenu]);
+
   return (
     <main className={`${inter.className} ${className}`}>
       <Header />
