0504ce41ecb805c77ec8ae23b37b0cfe8d3896d3
Elif Çiftçi <eliffbat@gmail.com>
2024-04-16 23:03:25 +0300

    refactor: sidemap link updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://click-craft.vercel.app/",
+      url: "https://click-and-craft.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://click-craft/craft.vercel.app/",
+      url: "https://click-and-craft/craft.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
