57c7715db752b3542e879a3f287e84b9442fc96e
Elif <eliffbat@gmail.com>
2024-05-09 17:59:40 +0300

    chore: page name updated to keep changes

---
+++ src/app/craft1/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import { inter } from "../fonts";
+import Preview from "@/component/organisms/Preview";
+import CraftMenu from "@/component/organisms/CraftMenu";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Click and Craft",
+  description: "Click items to craft your own website",
+};
+
+export default function NoCode() {
+
+  return (
+    <div className={`${inter.className} relative h-[100vh] bg-black-default mx-auto divide-y selection:bg-blue-lighter selection:text-black-darker`}>
+      <Preview />
+      <CraftMenu />
+    </div>
+  );
+}
