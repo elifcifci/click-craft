ce5c57e56c2f77d59328424ee993d65903a323f8
Elif <eliffbat@gmail.com>
2024-05-09 20:04:29 +0300

    chore: scrollPoint calculated

--- src/utils/scrollPageUtil.ts
+++ src/utils/scrollPageUtil.ts
@@ -1,6 +1,6 @@
 export const scrollPageUtil = (element: HTMLElement) => {
   if (typeof window !== "undefined") {
-    const scrollPoint = element.offsetTop;
+    const scrollPoint = element.offsetTop - 80;
     window.scrollTo({ top: scrollPoint, behavior: "smooth" });
   }
 };
